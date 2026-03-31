7b519291546a3d7779a8127a15e39d7e63e52c44
Bonzbear <awidjaya237@gmail.com>
2026-03-31 13:42:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ def format_telegram(df):
 
         ticker = row["Ticker"].replace(".JK","")
 
-        msg += f"{ticker} | {row['Signal']}\n"
-        msg += f"Close: {row['Close']} | {row['Change%']}% | Score: {row['Score']}\n\n"
+        msg += f"{ticker} | Close: {row['Close']} | {row['Change%']}% | Score: {row['Score']}\n\n "
 
     return msg
 

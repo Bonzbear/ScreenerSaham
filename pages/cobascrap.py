7682c5b3677799ebcb736abcc8c8d2f3b046c0ec
Bonzbear <awidjaya237@gmail.com>
2026-04-21 11:26:24 +0700

    Update cobascrap.py

--- pages/cobascrap.py
+++ pages/cobascrap.py
@@ -213,8 +213,8 @@ def is_signal(df):
         today["Volume"] > prev["Volume"] and
         prev["Close"] < today["Close"] and
         today["Close"] > today["SMA5"] and
-        today["Value"] > 10_000_000_000 and
-        today["ValueRatio"] > 2
+        today["Value"] > 10_000_000_000
+        #today["ValueRatio"] > 2
     ):
         return False
 
